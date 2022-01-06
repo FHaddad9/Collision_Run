84c5828862d82fd1d073918949c50b1bf0567df6
FHaddad9 <ferashaddadgit@gmail.com>
2022-01-06 00:57:49 +0000

    Game Screen added

---
+++ src/GamePanel.java
@@ -0,0 +1,114 @@
+
+import java.awt.Color;
+import java.awt.Dimension;
+import java.awt.Graphics;
+import java.awt.Graphics2D;
+
+import javax.swing.JPanel;
+
+public class GamePanel extends JPanel implements Runnable{
+	
+	// screen dimensions
+	final int normalTileSize = 16;	// 16x16 tiles, basically since it's 16 bit game
+	final int scale = 3;	// Enlarge the 16 pixels
+	final int tileSize = normalTileSize * scale;	// Scaled tile
+	
+	// Ratio 4:3 screen
+	final int screenCol = 16;
+	final int screenrow = 12;
+	final int screenWidth = tileSize * screenCol;	// 768 pixel
+	final int screenHeight = tileSize * screenrow;	// 576 pixel
+	
+	// Controls the time of the game; starting and stopping
+	Thread gameThread;
+	
+	// Gets keys pressed by user
+	KeyHandler key = new KeyHandler();
+	
+	// Player's default position
+	int posX = 350;
+	int posY = 500;
+	int speed = 4;
+	
+	// Frames per second
+	int fps = 60;
+	
+	public GamePanel() {
+		
+		// Constructor for the window
+		this.setPreferredSize(new Dimension(screenWidth, screenHeight));
+		this.setBackground(Color.black);
+		this.setDoubleBuffered(true);
+		this.addKeyListener(key);
+		this.setFocusable(true);
+	}
+
+	public void startGameThread() {
+		
+		// Starts the game by calling the run method
+		gameThread = new Thread(this);
+		gameThread.start();
+	}
+	
+	public void run() {
+		
+		// Update: update information
+		// Draw: draw screen with information
+		
+		// Interval is 1 second in 1 billion nanoseconds
+		double interval = 1000000000 / fps;	// 0.01666 seconds
+		double delta = 0;
+		
+		long lastTime = System.nanoTime();
+		long currentTime;
+		long timer = 0;
+		int drawCount = 0;
+		
+		// Delta gets the interval between the time when loop started and current time and repaints
+		while(gameThread != null) {
+			
+			currentTime = System.nanoTime();
+			
+			delta += (currentTime - lastTime) / interval;
+			timer += (currentTime - lastTime);
+			lastTime = currentTime;
+			
+			if(delta >= 1) {
+				update();
+				repaint();
+				delta--;
+				drawCount++;
+			}		
+			
+			if(timer >= 1000000000) {
+				//System.out.println("FPS: " + drawCount);
+				drawCount = 0;
+				timer = 0;
+			}
+		}
+	}
+	
+	public void update() {
+		
+		// When user presses key, player position moves by 4 (speed) pixels
+		if(key.left) {
+			posX -= speed;
+		} else if(key.right) {
+			posX += speed;
+		}
+	}
+	
+	public void paintComponent(Graphics g) {
+		
+		super.paintComponent(g);
+		
+		// Graphics2D better than Graphics
+		Graphics g2 = (Graphics2D)g;
+		
+		// Temporary "Character" for game. Which is just a white square :p
+		g2.setColor(Color.white);		
+		g2.fillRect(posX, posY, tileSize, tileSize);
+		
+		g2.dispose();
+	}
+}
---
+++ src/KeyHandler.java
@@ -0,0 +1,43 @@
+
+import java.awt.event.KeyEvent;
+import java.awt.event.KeyListener;
+
+public class KeyHandler implements KeyListener {
+	
+	public boolean left, right;
+
+	@Override
+	public void keyTyped(KeyEvent e) {
+		
+	}
+
+	@Override
+	public void keyPressed(KeyEvent e) {
+		
+		int code = e.getKeyCode();
+
+		switch (code) {
+			case KeyEvent.VK_LEFT:
+				left = true;
+				break;
+			case KeyEvent.VK_RIGHT:
+				right = true;
+				break;
+		}
+	}
+
+	@Override
+	public void keyReleased(KeyEvent e) {
+		int code = e.getKeyCode();
+		
+		switch (code) {
+			case KeyEvent.VK_LEFT:
+				left = false;
+				break;
+			case KeyEvent.VK_RIGHT:
+				right = false;
+				break;
+		}
+	}
+
+}
